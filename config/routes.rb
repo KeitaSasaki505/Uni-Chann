91a3803d9173c25fae8ac76cf1c9e4783f07a829
KeitaSasaki505 <cutba505@gmail.com>
2020-10-08 15:10:00 +0900

    テーブル機能およびベース実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :tweets
+  root to: 'tweets#index'
+  resources :tweets do
+    resources :comments
+    collection do
+      get 'search'
+    end
+  end
+  resources :users
+  
 end
